53398de929b066e9896f8a06070d48458fdfbe9c
Yourcreat <107115804+Yourcreat@users.noreply.github.com>
2026-07-11 21:50:31 +0530

    Add project management functions in projectManager.js

--- services/projectManager.js
+++ services/projectManager.js
@@ -3,3 +3,36 @@ const projects = {};
 function save(chatId, data) {
   projects[chatId] = data;
 }
+
+function get(chatId) {
+  return projects[chatId] || null;
+}
+
+function update(chatId, newData) {
+
+  if (!projects[chatId]) {
+    projects[chatId] = {};
+  }
+
+  projects[chatId] = {
+    ...projects[chatId],
+    ...newData
+  };
+
+}
+
+function clear(chatId) {
+  delete projects[chatId];
+}
+
+function exists(chatId) {
+  return !!projects[chatId];
+}
+
+module.exports = {
+  save,
+  get,
+  update,
+  clear,
+  exists
+};

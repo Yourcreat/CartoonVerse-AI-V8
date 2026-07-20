8d43dd72299ef1c697a4ac44d7c6d26a33be0352
Yourcreat <107115804+Yourcreat@users.noreply.github.com>
2026-07-20 22:39:00 +0530

    Add OpenMontage video generation function

--- services/videoProviders/openmontage.js
+++ services/videoProviders/openmontage.js
@@ -1,1 +1,16 @@
+const axios = require("axios");
 
+async function generateVideo(prompt) {
+
+    return {
+        success: false,
+        provider: "OpenMontage",
+        model: "V15",
+        message: "OpenMontage not connected yet."
+    };
+
+}
+
+module.exports = {
+    generateVideo
+};

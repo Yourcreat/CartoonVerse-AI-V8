15ab11066792f80853d033b366b9a541feecdcad
Yourcreat <107115804+Yourcreat@users.noreply.github.com>
2026-07-21 22:26:49 +0530

    Create falvideo.js

---
+++ services/videoProviders/falvideo.js
@@ -0,0 +1,30 @@
+const fal = require("@fal-ai/client");
+
+fal.config({
+    credentials: process.env.FAL_KEY
+});
+
+async function generateVideo(prompt) {
+
+    const result = await fal.subscribe(
+        "fal-ai/ltx-video-v097",
+        {
+            input: {
+                prompt: prompt
+            }
+        }
+    );
+
+    return {
+        success: true,
+        provider: "Fal AI",
+        model: "LTX-Video",
+        video: result.data.video.url,
+        prompt
+    };
+
+}
+
+module.exports = {
+    generateVideo
+};
